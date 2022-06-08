0d6f08e31881ef3de022daa63962913bac22d51f
Dmitry Weiner <dmitry.weiner@gmail.com>
2022-06-08 14:13:45 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,59 @@
+const SERVER_URL = "http://localhost:3000/";
+const TIMEOUT = 1000;
+
+let items = [
+    /*{
+        id: "123123123",
+        username: "Вася Пупкин",
+        text: "Всем привет, я Вася!",
+        timestamp: 123123123123
+    },*/
+];
+
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const message = document.getElementById("message");
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    sendMessage(username.value, message.value);
+    message.value = "";
+});
+
+function render() {
+    let result = "";
+    list.innerHTML = "";
+    for (const item of items) {
+        result += `<li>
+                <span class="username">${item.username}:</span>
+                <span>${item.text}</span>
+            </li>`;
+    }
+    list.innerHTML = result;
+}
+
+render();
+
+async function sendMessage(username, text) {
+    const data = {
+        username,
+        text
+    };
+    await fetch(SERVER_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+    fetchMessages();
+}
+
+async function fetchMessages() {
+    const response = await fetch(SERVER_URL);
+    items = await response.json();
+    render();
+    setTimeout(fetchMessages, TIMEOUT);
+}
+
+fetchMessages();
